41eb04db92565c2465c59502be8650258adc1314
Daniel <daniel@pokoapp.xyz>
2023-10-30 11:35:05 +0700

    testing refiner

--- app/stable_diffusion/manager/manager.py
+++ app/stable_diffusion/manager/manager.py
@@ -1,13 +1,12 @@
 import os
 import typing as T
 import torch
-from loguru import logger
 
 torch.backends.cudnn.benchmark = True
 import sys
 from random import randint
 from service_streamer import ThreadedStreamer
-from diffusers import AutoPipelineForText2Image, DPMSolverMultistepScheduler, AutoPipelineForImage2Image, AutoPipelineForInpainting
+from diffusers import DiffusionPipeline
 
 from app.stable_diffusion.manager.schema import (
     InpaintTask,
@@ -37,42 +36,55 @@ def build_pipeline(repo: str, device: str, enable_attention_slicing: bool):
         dump_path = repo[:-5]
         repo = conver_ckpt_to_diff(ckpt_path=repo, dump_path=dump_path)
 
-    stable_diffusion_txt2img = AutoPipelineForText2Image.from_pretrained(
-        repo,
+    # stable_diffusion_txt2img = AutoPipelineForText2Image.from_pretrained(
+    #     repo,
+    #     torch_dtype=torch.float16,
+    #     variant="fp16",
+    #     use_safetensors=True,
+    #     # revision="fp16",
+    #     # custom_pipeline="lpw_stable_diffusion",
+    # )
+    base = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    base.to("cuda")
+    refiner = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        text_encoder_2=base.text_encoder_2,
+        vae=base.vae,
         torch_dtype=torch.float16,
-        variant="fp16",
         use_safetensors=True,
-        # revision="fp16",
-        # custom_pipeline="lpw_stable_diffusion",
+        variant="fp16",
     )
+    refiner.to("cuda")
 
-    stable_diffusion_img2img = AutoPipelineForImage2Image.from_pipe(
-        stable_diffusion_txt2img
-    )
-
-    stable_diffusion_txt2img.scheduler = DPMSolverMultistepScheduler.from_config(stable_diffusion_txt2img.scheduler.config)
-    stable_diffusion_txt2img.safety_checker = lambda images, clip_input: (images, False)
-
-    stable_diffusion_inpaint = AutoPipelineForInpainting.from_pipe(stable_diffusion_txt2img)
-
-    if enable_attention_slicing:
-        stable_diffusion_txt2img.enable_attention_slicing()
+    # stable_diffusion_img2img = AutoPipelineForImage2Image.from_pipe(
+    #     stable_diffusion_txt2img
+    # )
+    #
+    # stable_diffusion_txt2img.scheduler = DPMSolverMultistepScheduler.from_config(stable_diffusion_txt2img.scheduler.config)
+    # stable_diffusion_txt2img.safety_checker = lambda images, clip_input: (images, False)
+    #
+    # stable_diffusion_inpaint = AutoPipelineForInpainting.from_pipe(stable_diffusion_txt2img)
+    #
+    # if enable_attention_slicing:
+    #     stable_diffusion_txt2img.enable_attention_slicing()
 
     # pipe.enable_model_cpu_offload()
     # pipe.unet = torch.compile(pipe.unet, mode="reduce-overhead", fullgraph=True)
-    stable_diffusion_txt2img = stable_diffusion_txt2img.to(device)
-    stable_diffusion_txt2img.load_lora_weights("./lora/anime_sdxl_v1.safetensors", weight_name="anime_sdxl_v1.safetensors",
-                           adapter_name='anime')
-    active_adapters = stable_diffusion_txt2img.get_active_adapters()
+    # stable_diffusion_txt2img = stable_diffusion_txt2img.to(device)
+    # stable_diffusion_txt2img.load_lora_weights("./lora/anime_sdxl_v1.safetensors", weight_name="anime_sdxl_v1.safetensors",
+    #                        adapter_name='anime')
+    # active_adapters = stable_diffusion_txt2img.get_active_adapters()
 
-    stable_diffusion_img2img = stable_diffusion_img2img.to(device)
-    stable_diffusion_inpaint = stable_diffusion_inpaint.to(device)
+    # stable_diffusion_img2img = stable_diffusion_img2img.to(device)
+    # stable_diffusion_inpaint = stable_diffusion_inpaint.to(device)
 
-    logger.info(f"{active_adapters}")
+    # logger.info(f"{active_adapters}")
     return dict(
-        text2img=stable_diffusion_txt2img,
-        img2img=stable_diffusion_img2img,
-        inpaint=stable_diffusion_inpaint,
+        text2img=base,
+        img2img=refiner,
+        inpaint=refiner,
     )
 
 
@@ -109,12 +121,28 @@ class StableDiffusionManager:
 
         device = env.CUDA_DEVICE
 
-        generator = self._get_generator(task, device)
-        task = task.dict()
-        del task["seed"]
-        images = pipeline(**task, generator=generator).images
-        if device != "cpu":
-            torch.cuda.empty_cache()
+        n_steps = 40
+        high_noise_frac = 0.8
+
+        image = pipeline(
+            prompt='8k picture of new york photoshoot with lisa from blackpink in street style, photorealistic, highly detailed',
+            num_inference_steps=40,
+            denoising_end=high_noise_frac,
+            output_type="latent",
+        ).images
+        images = self.pipe['img2img'](
+            prompt='8k picture of new york photoshoot with lisa from blackpink in street style, photorealistic, highly detailed',
+            num_inference_steps=n_steps,
+            denoising_start=high_noise_frac,
+            image=image,
+        ).images
+
+        # generator = self._get_generator(task, device)
+        # task = task.dict()
+        # del task["seed"]
+        # images = pipeline(**task, generator=generator).images
+        # if device != "cpu":
+        #     torch.cuda.empty_cache()
 
         return [images]
 
